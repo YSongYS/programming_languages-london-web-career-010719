754bb2d53ea82dd29d6f23440e741e1f934df797
Songyishu Yang <songyishu.yang@gmail.com>
2019-01-05 19:06:24 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,10 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = Hash.new
+  languages.each do |style, styleinfo|
+    styleinfo.each do |lan,laninfo|
+      new_hash[lan] = laninfo
+      new_hash[lan][style] = style
+    end
+  end
+  new_hash
 end
