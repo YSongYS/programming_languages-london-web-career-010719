2580cf122e66b4f034cb133d4a09d84cbbb01222
Songyishu Yang <songyishu.yang@gmail.com>
2019-01-05 19:11:20 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,8 +2,12 @@ def reformat_languages(languages)
   new_hash = Hash.new
   languages.each do |style, styleinfo|
     styleinfo.each do |lan,laninfo|
-      new_hash[lan] = laninfo
-      new_hash[lan][:style] = style
+      if new_hash.has_key?(lan)
+        new_hash[lan][:style] << style
+      else
+        new_hash[lan] = laninfo
+        new_hash[lan][:style] = style
+      end
     end
   end
   puts new_hash
